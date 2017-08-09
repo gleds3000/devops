108223e7e63270be7d5a0a7bf8ab2988337b107d
Gleds3000 <gleds3000@gmail.com>
2017-08-09 16:48:15 -0300

    teste
    teste

---
+++ matematica.java
@@ -0,0 +1,24 @@
+public class Matematica
+{ // corpo da classe
+ 
+  public static int a, b; // atributos da classe
+ 
+// adicao, subtracao, multiplicacao e divisao representam o "protocolo"
+// de métodos implementados pela classe Matematica.
+  public static int adicao() {
+    return (a + b);
+  }
+ 
+  public static int subtracao() {
+    return (a - b);
+  }
+ 
+  public static int multiplicacao() {
+    return (a * b);
+  }
+ 
+  public static int divisao() {
+    return (a / b);
+  }
+ 
+} // fim do corpo da classe "Matematica"
